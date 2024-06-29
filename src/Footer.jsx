afb711a4f68c3276802b1f1bdcbafb884fb058d9
DEEPAK YADAV <103742220+deepak212001@users.noreply.github.com>
2024-06-29 21:56:49 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,8 +4,16 @@ import './Footer.css';
 function Footer() {
   return (
     <footer >
-        
-        <span className='text-white'>Made with ❤️ by @Deepak</span>
+        <div className="foot">
+
+          <span id="mad">Made with ❤️ by @Deepak</span>
+          <span className="acc">
+            <a href="https://www.linkedin.com/in/deepakyadav24/" target="_blank"><img src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png" alt="linkedin"/></a>
+            <a href="https://github.com/deepak212001" target="_blank"><img src="https://img.icons8.com/ios-glyphs/30/FFFFFF/github.png" alt="github"/></a>
+            <a href="https://leetcode.com/u/deepakyadav21/" target="_blank"><img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/FFFFFF/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo.png" alt="external-level-up-your-coding-skills-and-quickly-land-a-job-logo-bold-tal-revivo"/></a>
+          </span>
+          
+        </div>
 
     </footer>
   )
